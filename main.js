4b99ff60b4e40905c48974b522e783119f420bbc
Meho Mujicic <mujicic.meho@gmail.com>
2018-12-20 23:24:30 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+var vpWidth = $(window).innerWidth();
+var vpHeight = $(window).innerHeight();
+var vid = $(".vid-bg").innerHeight();
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    $(window).scroll(function () {
+        $(".slideanim").each(function () {
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+})
+
+
+
+$(window).on("scroll", function () {
+    var scrollPos = $(window).scrollTop();
+    if (scrollPos < vid) {
+        $(".navbar").css("background-color", "transparent");
+        $(".navbar").fadeIn();
+    } else if (scrollPos > vid) {
+        $(".navbar").css("background-color", "#040404");
+        $(".navbar").fadeIn();
+
+    }
+});
